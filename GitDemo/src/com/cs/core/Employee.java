b44619ebf60833bc6157ccc0e434cf5b1320b21b
ganeshshinde1 <ganeshshinde2681998@gmail.com>
2021-09-15 03:53:28 -0700

    added the codebase

---
+++ GitDemo/src/com/cs/core/Employee.java
@@ -0,0 +1,12 @@
+package com.cs.core;
+
+public class Employee {
+
+	static String empName = "GANESH SIR";
+
+	public static void main(String[] args) {
+
+		System.out.println("\n Employee Name: " + empName);
+
+	}
+}
